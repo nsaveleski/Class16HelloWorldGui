a47306889d2f49f228f08472bf8c2ae99bc47eda
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2015-10-22 21:02:59 -0400

    The hello world app with coin image!

--- HelloWorldGui/src/cis232/helloworld/HelloWorldController.java
+++ HelloWorldGui/src/cis232/helloworld/HelloWorldController.java
@@ -3,6 +3,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloWorldController {
 
@@ -11,10 +13,17 @@ public class HelloWorldController {
 
     @FXML
     private Label label;
+    
+    @FXML
+    private ImageView image;
 
     @FXML
     void buttonClickListener(ActionEvent event) {
     	label.setVisible(true);
+    	Image coin = new Image(getClass().getResourceAsStream("coin.jpg"));
+    	//You could use a URL!
+    	//Image coin = new Image("https://upload.wikimedia.org/wikipedia/commons/0/0a/George_Washington_Presidential_$1_Coin_obverse.png");
+    	image.setImage(coin);
     }
 
 }
